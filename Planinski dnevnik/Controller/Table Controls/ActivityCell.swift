6c5d3eee0e1096815ac64c01f9c0dc2d6c38fe47
dmarkslo <mark.david.horvat@gmail.com>
2025-12-04 11:55:48 +0100

    ProfileView, Journal, work in progress, backend dela

---
+++ Planinski dnevnik/Controller/Table Controls/ActivityCell.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityCell.swift
+//  Planinski dnevnik
+//
+//  Created by Mark Horvat on 23. 11. 25.
+//
+
+import UIKit
+
+class ActivityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
